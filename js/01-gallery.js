867b6a437b195422b5db483ee7ebc543378e465f
puerarongo <akhardin96@gmail.com>
2022-01-26 20:15:59 +0300

    Exercise 1. connecting the basicLightbox library

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery")
+
+// todo Stage 1
+
+galleryItems.forEach(elem => {
+    // * Stage 1.1
+    const itemEl = document.createElement("div");
+    itemEl.classList.add("gallery__item");
+    galleryEl.append(itemEl);
+
+    // * Stage 1.2
+    const galleryLinkEl = document.createElement("a");
+    galleryLinkEl.classList.add("gallery__link");
+    galleryLinkEl.setAttribute("href", `${elem.original}`);
+    itemEl.append(galleryLinkEl);
+
+    // * Stage 1.3
+    const imageEl = document.createElement("img");
+    imageEl.classList.add("gallery__image");
+    imageEl.setAttribute("src", `${elem.preview}`);
+    imageEl.setAttribute("data-source", `${elem.original}`)
+    imageEl.setAttribute("alt", `${elem.description}`);
+    galleryLinkEl.append(imageEl);
+});
+
+// todo Stage 2
+
+galleryEl.addEventListener("click", selectPicture);
+
+function selectPicture(event) {
+    event.preventDefault();
+
+    if (event.target.nodeName === "IMG") {
+        const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`)
+
+instance.show()
+        return console.log("active")
+    }
+}
+
+
+
+
+
+
---
+++ twst666.js
@@ -0,0 +1,19 @@
+let a = 0
+
+console.log(a++)
+
+
+
+
+
+
+
+
+//let a = {},
+//    b = {key: 'b'},
+//    c = {key: 'c'};
+//
+//a[b] = 5;
+//a[c] = 14; 
+//
+//console.log(a[b]);
