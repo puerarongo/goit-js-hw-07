518f2a4efdb8bcd5bbe91dd1ac39e31dfe65e1d0
puerarongo <akhardin96@gmail.com>
2022-02-02 01:17:06 +0300

    Delete test1 file

--- js/test1.js
+++
@@ -1,5 +0,0 @@
-const instance = basicLightbox.create(`
-    <img src="https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg" width="800" height="600">
-`)
-
-instance.show()
