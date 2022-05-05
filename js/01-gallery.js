37c25c1d678e1e63f79b2ff0e3015c1db5dbaf58
puerarongo <akhardin96@gmail.com>
2022-05-05 08:49:00 +0300

    fiix

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -31,48 +31,7 @@ galleryEl.insertAdjacentHTML("beforeend", galleryInnerEl);
 galleryEl.addEventListener("click", selectPicture);
 
 
-const instance = basicLightbox.create(`
-<div class="modal">
-      <div class="modal__section">
-        <div class="modal__poster">
-          <img class="modal__img" src=${"https://content.rozetka.com.ua/goods/images/big/177447689.jpg"} alt="movie_poster" />
-        </div>
-        <div class="modal__container">
-          <h1 class="modal__title">Movie Name</h1>
-          <div class="modal__information">
-            <ul class="modal__item">
-              <li class="modal__into"><p>Vote/Votes:</p></li>
-              <li class="modal__into"><p>Popularity</p></li>
-              <li class="modal__into"><p>Original title</p></li>
-              <li class="modal__into"><p>Genre</p></li>
-            </ul>
-            <ul>
-              <li class="modal__into modal__into--vote">
-                  <span class="modal__vote">7.3</span> / <span class="modal__votes">1260</span>
-              </li class="modal__into">
-              <li class="modal__into"><p>100.2</p></li>
-              <li class="modal__into"><p class="modal__origin">A FISTFUL OF LEAD</p></li>
-              <li class="modal__into"><p>Western</p></li>
-            </ul>
-          </div>
-          <h2 class="modal__subtitle">About</h2>
-          <p class="modal__text">
-            Four of the West’s most infamous outlaws assemble to steal a huge
-            stash of gold from the most corrupt settlement of the gold rush
-            towns. But not all goes to plan one is killed and the other three
-            escapes with bags of gold hide out in the abandoned gold mine where
-            they happen across another gang of three – who themselves were
-            planning to hit the very same bank! As tensions rise, things go from
-            bad to worse as they realise the bags of gold are filled with
-            lead... they’ve been double crossed – but by who and how?
-          </p>
-          <div class="modal__buttons">
-            <button class="button">Add to watched</button>
-            <button class="button">Add to queue</button>
-          </div>
-        </div>
-      </div>
-    </div>`,
+const instance = basicLightbox.create(`<img class="picture__lightbox">`,
   {
     onShow: (condition) => {
       window.addEventListener("keydown", keyboardEvent)
@@ -89,25 +48,19 @@ const instance = basicLightbox.create(`
 function selectPicture(event) {
     event.preventDefault();
 
-
+  if (event.target.nodeName === "IMG") {
+    instance.element().querySelector(".picture__lightbox")
+      .setAttribute("src", `${event.target.dataset.source}`)
 
     instance.show()
-  
+  };
 }; 
 
 function keyboardEvent(event) {
   if (event.key === "Escape") {
     instance.close()
-    console.log("!")
   }
 }
-
-//if (event.target.nodeName === "IMG") {
-//    instance.element().querySelector(".picture__lightbox")
-//      .setAttribute("src", `${event.target.dataset.source}`)
-//
-//    instance.show()
-//  };
 
 
 
